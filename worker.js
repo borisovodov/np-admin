a934dc681331a7beac00b92b69e22cbf1c21fbfd
Boris Ovodov <boris@ovodov.me>
2023-12-03 12:52:45 +0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,31 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+// Это работа с модулями, без которой не работает D1 и вебхуки работают через листнеры — то бишь через жопу.
 export default {
-  async fetch(request, env, ctx) {
-    return new Response('Hello World!');
+  // Это точка входа. Мы получаем запрос, который будем в дальнейшем обрабатывать, переменные окружения и контекст выполнения кода.
+  async fetch(request, env, context) {
+    // Это пример того, как полуются переменные окружения.
+    //const BOT_TOKEN = "${env.BOT_TOKEN}"
+
+    // Преобразуем полученный запрос в JSON-объект.
+    let body = JSON.parse(JSON.stringify(await request.json()));
+
+    // Формируем содержимое ответа на запрос.
+    let answer = {
+           "method":"sendMessage",
+           "chat_id": body.message.chat.id,
+           "reply_to_message_id" : body.message.message_id,
+           "text" :JSON.stringify(body.message)
+    };
+
+    // Формируем ответ на запрос.
+    let response = new Response(
+      JSON.stringify(answer), {
+        headers: {
+          "content-type": "application/json;charset=UTF-8",
+        },
+        status: 200
+      })
+    
+      // Возвращаем ответ на запрос.
+    return response
   },
 };
